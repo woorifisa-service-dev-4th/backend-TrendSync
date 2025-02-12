f4a19f0038170d8748ff0fec499e61a85904ffc0
bonun00 <lifesciences00@naver.com>
2025-02-12 10:16:09 +0900

    로컬 변경 사항 커밋

--- src/main/java/dev/trend/domain/news/News.java
+++ src/main/java/dev/trend/domain/news/News.java
@@ -19,9 +19,66 @@ public class News {
     private String imgUrl;
 
     /**뉴스 조회수 */
-    private Long views;
+    private Long views=0L;
 
     /**뉴스 카테고리*/
     private Category category;
 
+
+    public News(Long newsId, String title, String content, String imgUrl, Long views, Category category) {
+        this.newsId = newsId;
+        this.title = title;
+        this.content = content;
+        this.imgUrl = imgUrl;
+        this.views = views;
+        this.category = category;
+    }
+
+    public Long getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(Long newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getViews() {
+        return views;
+    }
+
+    public void setViews(Long views) {
+        this.views = views;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- src/main/java/dev/trend/repository/NewsRepository.java
+++ src/main/java/dev/trend/repository/NewsRepository.java
@@ -1,4 +1,26 @@
 package dev.trend.repository;
 
+import dev.trend.domain.news.News;
+
+import java.awt.*;
+
 public class NewsRepository {
+
+    News news =new News();
+
+
+    public List<String> FindAllNews(int id){
+
+
+        return ;
+
+    }
+
+
+
+
+
+
+
+
 }
